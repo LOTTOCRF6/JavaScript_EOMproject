64d94aac234793f0c0515a848526566511700a31
lifechoices <sithandathuzipho@gmail.com>
2021-08-13 16:13:11 +0200

    fiveth commit

--- JS/products.js
+++ JS/products.js
@@ -1,248 +1,46 @@
-// const data = [
-//     {
-//       product: "Haori Jacket",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d4f67e4b00b2c2a29ab00/1443712877696/lauren-winter-haori-jacket_0250.jpg?format=750w",
-//       altDesc: "Jacket",
-//       price: "$210.00",
-//       category: "outwear"
-//     },
-//     {
-//       product: "Swing Dress",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d5226e4b0e3eb29871ecf/1443713579307/lauren-winter-swing-dress_0183.jpg?format=750w",
-//       altDesc: "Dress",
-//       price: "$218.00",
-//       category: "dresses"
-//     },
-//     {
-//       product: "Linen Top",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d542ae4b088b5adb66691/1443714094740/ulihu-blue-linen-crop-top_0320.jpg?format=750w",
-//       altDesc: "Blouse",
-//       price: "$125.00",
-//       category: "tops"
-//     },
-//     {
-//       product: "Lounge Tunic",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d5093e4b08652fbed4bf8/1443713176495/lauren-winter-natural-dress_0172.jpg?format=750w",
-//       altDesc: "Dress",
-//       price: "$258.00",
-//       category: "dresses"
-//     },
-//     {
-//       product: "Sonia Dress",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d50c4e4b007e3085421e7/1443713227153/lauren-winter-sonia-dress-terracotta_0228.jpg?format=750w",
-//       altDesc: "Dress",
-//       price: "$268.00",
-//       category: "dresses"
-//     },
-//     {
-//       product: "Swing Top",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d51a6e4b079ec66a5ba17/1443713452396/lauren-winter-studio-top-natural_0190.jpg?format=2500w",
-//       altDesc: "Blouse",
-//       price: "$128.00",
-//       category: "tops"
-//     },
-//     {
-//       product: "Terra Cota Studio Top",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d51ebe4b0edc898c465c1/1443713520778/lauren-winter-studio-top-terracotta_0222.jpg?format=750w",
-//       altDesc: "Blouse",
-//       price: "$158.00",
-//       category: "tops"
-//     },
-//     {
-//       product: "Poplin Chef Shirt",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560c5e1be4b0a3995ef06901/1443651103753/kimem-poplin-chef-shirt-white_0304.jpg?format=750w",
-//       altDesc: "Shirt",
-//       price: "$216.00",
-//       category: "tops"
-//     },
-//     {
-//       product: "Poplin Dress",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d4bcee4b07d1ff8f02586/1443711955781/kimem-poplin-dress-blue_0289.jpg?format=750w",
-//       altDesc: "Dress",
-//       price: "$231.00",
-//       category: "dresses"
-//     },
-//     {
-//       product: "Layer Dress",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d4c1ee4b025c17c7ad058/1443712033979/kimem-layer-dress-white_0168.jpg?format=750w",
-//       altDesc: "Dress",
-//       price: "$309.00",
-//       category: "dresses"
-//     },
-//     {
-//       product: "3 Button Tunic",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d54dde4b0c3e37b4be368/1443714275625/ulihu-blue-linen-tunic_0308.jpg?format=750w",
-//       altDesc: "Blouse",
-//       price: "$250.00",
-//       category: "dresses"
-//     },
-//     {
-//       product: "Dusty Jumpsuit",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d4d0de4b0edc898c43a14/1443712276073/kimem-dusty-jumpsuit_0358.jpg?format=750w",
-//       altDesc: "Jumpsuit",
-//       price: "$299.00",
-//       sale: "$399.00",
-//       category: "dresses"
-//     },
-//     {
-//       product: "Lisa Shirt",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d4d67e4b094d8b81ba47a/1443712364244/kimem-lisa-oversized-shirt-navy_0363.jpg?format=750w",
-//       altDesc: "Shirt",
-//       price: "$253.00",
-//       category: "tops"
-//     },
-//     {
-//       product: "Romy Top",
-//       url: "",
-//       image: "https://static1.squarespace.com/static/560c458be4b0af26f729d191/560c5de0e4b083d9c365515f/560d4e8be4b08ac15504170b/1443712656147/kimem-romy-dolman-top_0146.jpg?format=750w",
-//       altDesc: "Blouse",
-//       price: "$199.00",
-//       sale: "$300.00",
-//       category: "tops"
-//     },
-//   ];
-// fetch(https://protected-lake-94601.herokuapp.com/Get-Products/)
-//     .then(res => res.json())
-//     .then(data => {
-//         console
-//     })
-  
-  // Generate template literal
-  function clothingView(item, index) {
-    return (`
-      <a href="${item.url}" class="shop-item">
-      ${item.sale ? `<span class="shop-item__sale">Sale</span>`: ''}
-        <img src=${item.image} alt="${item.altDesc}" class="shop-item__img">
-        <div class="quickview">
-          <span class="quickview__icon">Quick View</span>
-          <span class="quickview__info">${item.product}
-            <br>
-            <span class="quickview__price">${item.price}
-              ${item.sale ? `<span class="quickview__price--sale">${item.sale}</span>`: ''}
-            </span>
-          </span>
-          <span class="shop-item__index">${index}</span>
+
+let products = [];
+let cart = [];
+
+fetch("http://protected-lake-94601.herokuapp.com/Get-Products/")
+  .then((res) => res.json())
+  .then((data) => {
+    products = data;
+    console.log(data);
+    make_products(data);
+  });
+
+function make_products(products) {
+  let product_container = document.querySelector("#products-container");
+  product_container.innerHTML = "";
+  products.data.forEach((product) => {
+    product_container.innerHTML += `
+        <div class = "products">
+            <img src="${product.product_image}" class = "product-image"> 
+            <h4 class = "product-title"> ${product.product_name}</h4>
+            <p class = "product-description"> ${product.description}</p>
+            <p class = "product-price">${product.price} </p>
+            <button onclick="addToCart(${product.id})">Add to Cart</button>
+            
         </div>
-      </a>
-      `);
-  };
-  
-  // Append template literal to html structure based on category
-  data.map((item, index) => {
-    const showItems = clothingView(item, index);
-  
-    $('.all-items').append(showItems);
-    $('.' + item.category).append(showItems);
-    item.sale ? $('.sale').append(showItems) : null
+    `;
   });
-  
-  // Filtering data from array based on category
-  let navCategoryClicked; // This is to check if categories__link was clicked
-  let filteredData = [];
-  
-  $('.categories__link').click(function() {
-    navCategoryClicked = true;
-    const category = this.id;
-    let filteringData;
-  
-    if (category === 'bottoms') {
-      filteringData = data.filter(el => el.category === 'bottoms');
-    } else if (category === 'tops') {
-      filteringData = data.filter(el => el.category === 'tops');
-    } else if (category === 'outwear') {
-      filteringData = data.filter(el => el.category === 'outwear');
-    } else if (category === 'accessories') {
-      filteringData = data.filter(el => el.category === 'accessories');
-    } else if (category === 'dresses') {
-      filteringData = data.filter(el => el.category === 'dresses');
-    } else {
-      filteringData = data.filter(el => el.sale);
-    }
-  
-    filteredData = filteringData;
-    console.log(filteredData);
-  });
-  
-  // Change clothing-item popup window img and info
-  const swapPopup = clothing => {
-    $('#clothingLink').prop('href', clothing.url)
-    $('#clothingImg').prop('src', clothing.image)
-    $('#clothingName').text(clothing.product)
-    $('#clothingPrice').text(clothing.price)
-    
-    // If clothing-item has sale property add it to popup.
-    clothing.sale ? $('#clothingSale').text(clothing.sale) : $('#clothingSale').text(null)
-  };
-  
-  // Change cloting-item img and info
-  let currentPopup;
-  $('.quickview__icon').click(function(e) {
-    event.preventDefault();
-    $('.overlay').css({'opacity': '1', 'visibility': 'visible'});
-  
-    currentPopup = $(e.target).parent().children('.shop-item__index').text();
-    swapPopup(data[currentPopup]);
-  });
-  
-  // Popup cloting-item prev and next buttons
-  let dataArr;
-  $('#popupPrev').click(function() {
-    // Change data based on whether categories__link was clicked or not
-    navCategoryClicked != true ? dataArr = data : dataArr = filteredData;
-    
-    currentPopup > 0 ? --currentPopup : currentPopup = dataArr.length - 1;
-    swapPopup(dataArr[currentPopup]);
-    console.log(currentPopup);
-  });
-  
-  $('#popupNext').click(function() {
-    // Change data based on whether categories__link was clicked or not
-    navCategoryClicked != true ? dataArr = data : dataArr = filteredData;
-    
-    currentPopup < dataArr.length -1 ? ++currentPopup : currentPopup = 0;
-    swapPopup(dataArr[currentPopup]);
-    console.log(currentPopup);
-  });
-  
-  // Close popup window
-    $('#closeIcon').click(function() {
-    $('.overlay').css({
-      'opacity': '0', 
-      'visibility': 'hidden'});
-    });
-  
-  // The code from here and down is not related to the code above
-  //Show clothing items nav sections
-    $('.categories__link').click(function(){
-  
-      function showingSection(e) {
-        $(e).fadeIn('slow');
-        $(e).css('display', 'flex');
-      };
-  
-      $('.products').css('display', 'none');
-      showingSection('.' + this.id);
-    });
-  
-  // Hide sale icon on clothing-item hovering
-    $('.shop-item').hover(
-      function() { // Mouse in
-      $('.shop-item__sale', this).fadeOut();
-      },
-      function() { // Mouse out
-      $('.shop-item__sale', this).fadeIn();
-      }
-    );
+}
+
+function searchProducts() {
+  let searchTerm = document.querySelector("#searchTerm").value;
+  console.log(searchTerm);
+
+  let searchedProducts = products.filter((product) => 
+    product_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  console.log(searchedProducts);
+
+}
+
+function toggleCart() {
+  document.querySelector("#cart").classList.toggle("active");
+}
+
+
+ 

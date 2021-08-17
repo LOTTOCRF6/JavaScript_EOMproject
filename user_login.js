63cc37b02f52d9eb30f45b0bddd6f86ed2d4f931
lifechoices <sithandathuzipho@gmail.com>
2021-08-17 15:51:04 +0200

    last commit

--- user_login.js
+++ user_login.js
@@ -1,33 +1,4 @@
-function Login() {
-  fetch("https://protected-lake-94601.herokuapp.com/auth/", {
-    method: "POST",
-    body: JSON.stringify({
-      username: document.getElementById("username").value,
-      password: document.getElementById("password").value,
-    }),
-    headers: {
-      "Content-type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      if (data["description"] == "Invalid credentials") {
-        alert(
-          "Username or password is incorrect. Please enter correct details"
-        );
-      } else {
-        console.log(data["access_token"]);
-        mystorage.setItem("jwt-token", data["access_token"]);
-        mystorage.setItem(
-          "username",
-          document.getElementById("username").value
-        );
-        window.location.href = "./products.html";
-      }
-    });
-}
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
 
-function register() {
-  document.querySelector(".signupcontainer").classList.toggle("active");
-}

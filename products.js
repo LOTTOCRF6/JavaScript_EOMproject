a4e9d6417d00d690ba34e7520f99d3179112846f
lifechoices <sithandathuzipho@gmail.com>
2021-08-18 14:44:05 +0200

    update

--- products.js
+++ products.js
@@ -1,6 +1,7 @@
 let products = [];
 let cart = [];
 
+// Show Products
 fetch("http://protected-lake-94601.herokuapp.com/Get-Products/")
   .then((res) => res.json())
   .then((data) => {
@@ -9,6 +10,7 @@ fetch("http://protected-lake-94601.herokuapp.com/Get-Products/")
     make_products(data);
   });
 
+// Making Products
 function make_products(products) {
   let product_container = document.querySelector("#products-container");
   product_container.innerHTML = "";
@@ -26,9 +28,10 @@ function make_products(products) {
   });
 }
 
+// Search
 function searchProducts() {
   let searchTerm = document.querySelector("#searchTerm").value;
-  console.log(searchTerm);
+  // console.log(searchTerm);
 
   let searchedProducts = products.data.filter((product) =>
     product.product_name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -37,7 +40,7 @@ function searchProducts() {
   let product_container = document.querySelector("#products-container");
   product_container.innerHTML = "";
   products.data.forEach((product) => {
-    console.log(product);
+    // console.log(product);
     product_container.innerHTML += `
         <div class = "products">
             <img src="${product.product_image}" class = "product-image"> 
@@ -49,8 +52,10 @@ function searchProducts() {
         </div>
     `;
   });
+  // make_products(searchedProducts);
 }
 
+// CARTS
 function toggleCart() {
   document.querySelector("#cart").classList.toggle("active");
 }
@@ -59,33 +64,46 @@ function renderCart(cartItems) {
   cartContainer.innerHTML = "";
   if (cartItems.length > 0) {
     cartItems.map((cartItem) => {
-      // console.log('This my carts',cartItem);
       cartContainer.innerHTML += `
       <div class = "products">
-      <img src="${cartItem.product_image}" class = "product-image"> 
-      <div class="product-content">
-          <h4 class = "product-title"> ${cartItem.product_name}</h4>
-          <p class = "product-description"> ${cartItem.description}</p>
-          <p class = "product-price">${cartItem.price} </p>
-      </div> 
-  </div>
+            <img src="${cartItem.product_image}" class = "product-image">
+            <div class = "product-content"> 
+                <h4 class = "product-title"> ${cartItem.product_name}</h4>
+                <p class = "product-description"> ${cartItem.description}</p>
+                <p class = "product-price">${cartItem.price} </p>
+                <button class ="revome_cart" onclick="removeItem(${cartItem.id})">Remove item</button>
+            </div>
+            
+        </div>
+      
+      
       `;
     });
-    let totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
-    cartContainer.innerHTML = +`<h2>Total is: ${totalPrice}</h2>`;
+    let totalPrice = cartItems.reduce((total, item) => total + item.price,0);
+    cartContainer.innerHTML += `<h3> Total is: ${totalPrice} </h3>`;
   } else {
     cartContainer.innerHTML = "<h2> No items in cart</h2>";
   }
 }
+
 function addToCart(id) {
-  // console.log(products.data);
+  console.log(products.data);
   let product = products.data.find((item) => {
     return item.id == id;
   });
-  console.log(product);
   cart.push(product);
-  console.log(cart);  
-  //console.log("your are cart items", cart);
+  renderCart(cart);
+}
+function removeItem(id) {
+  let product = products.data.find((item) => {
+    return item.id == id;
+  });
+  //console.log(product);
+
+  cart.splice(
+    cart.findIndex((a) => a.id === product.id),
+    1
+  );
   renderCart(cart);
 }
 
